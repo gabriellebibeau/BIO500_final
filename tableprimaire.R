4dbed2fdd6442f9b6479090c5c623bc4bf6851e2
EmmanuelleLanglois <manue.l270@hotmail.ca>
2024-03-10 15:35:25 -0400

    Update tableprimaire.R

--- tableprimaire.R
+++ tableprimaire.R
@@ -1,98 +1,105 @@
-setwd("C:/Users/manue/OneDrive - USherbrooke/Documents/Documents/Notes Université/Session 6 - UdeS/Méthodes computationnelles/GitHub/BIO500/BIO500_final/benthos")
+setwd("C:/Users/manue/OneDrive - USherbrooke/Documents/Documents/Notes Université/Session 6 - UdeS/Méthodes computationnelles/GitHub/BIO500/BIO500_final")
 
-# Ressource : https://econumuds.github.io/BIO500/sql.html
-#Pas necessaire de faire fonctions pour cette partie je pense?
+#Creation de la fonction qui permettra de produire nos tables de donnees pour la base de donnees relationnelles
+creation_bd <- function(donnees, dbname){
+  #Le fichier données dit contenir les mêmes noms de variables que notre fichier df.propre
+  #dbname correspond au chemin que l'ordinateur doit prendre pour retrouver le fichier que l'on veut créer. Donc, il s'agirait de setwd() en ajoutant à la fin de se dernier le nom du fichier contenant les tables de données et se terminant par .bd
+  install.packages("RSQLite") #Le package RSQLite permet de se connecter au serveur SQLite et de l'utiliser a partir de R afin de pouvoir creer une base de donnees relationnelles
+  
+  #Connection de notre fichier de donnees au serveur SQLite
+  bd <- dbConnect(RSQLite::SQLite(), dbname) 
+  
+  #Creer deux tables contenant dans l'ensemble toutes les donnees necessaires à l'analyse, liées entre elles par l'ID_site (clé étrangère).
+  
+  #table des abondances d'espece
+  creer_especes_par_site <- 
+    "CREATE TABLE especes_par_site (
+      
+      id_site VACHAR(100)
+      nom_sci VACHAR(100)
+      abondance INTEGER
+      fraction REAL
+      
+      PRIMARY KEY (id_site, nom_sci)
+      FOREIGN KEY (id_site) REFERENCES infos_sites(id_site)
+    );"
+  dbSendQuery(bd, creer_especes_par_site)
+  
+  #Creation table des sites
+  creer_infos_sites <-
+    
+    "CREATE TABLE infos_sites (
+      id_site             VARCHAR(15)
+      date_obs            VARCHAR(100)
+      heure_obs           DATETIME
+      site                VARCHAR(150)
+      largeur_riviere     INTEGER
+      profondeur_riviere  INTEGER
+      vitesse_courant     INTEGER
+      temperature_eau_c   REAL
+      transparence_eau    VARCHAR(15)
+      ETIQSTATION         VARCHAR(20)
+      
+      PRIMARY KEY (id_site)
+    );"
+  dbSendQuery(bd, creer_infos_sites)
+  
+  #Injection des donnees dans les tables SQLite
+  dbWriteTable()
+  
+  dbDisconnect()
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 #installer SQLite3 pour pouvoir creer les tableaux de donnees
 install.packages('RSQLite')
 install.packages("DBI")
-library("DBI")
+library(RSQLite)
 
 #Connection de notre fichier de donnees au serveur SQLite
-bd <- dbConnect(RSQLite::SQLite(), dbname="C:/Users/manue/OneDrive - USherbrooke/Documents/Documents/Notes Université/Session 6 - UdeS/Méthodes computationnelles/GitHub/BIO500/BIO500_final/benthos")
+bd <- dbConnect(RSQLite::SQLite(), dbname="./tables_donnees.db")
 
-#Creer trois tables contenant dans l'ensemble toutes les donnees necessaires au travail
+#Creer deux tables contenant dans l'ensemble toutes les donnees necessaires à l'analyse, liées entre elles par l'ID_site (clé étrangère)
 
-#Creation table donnant un id_espece a chaque espece differente observee a chaque site a une heure donnee et date donnee
+#table des abondances d'espece
 creer_especes_par_site <- 
   CREATE TABLE especes_par_site (
     
     id_site VACHAR(100)
-    date_obs DATE
-    heure_obs 
-    site VACHAR(150)
-    id_espece VACHAR(100)
-    
-    PRIMARY KEY (date_obs, heure_obs, site, AUTREEEEE)
-    FOREIGN KEY (id_site) REFERENCES infos_sites(id_site)
-    FOREIGN KEY (id_espece) REFERENCES infos_especes(id_espece)
-  )
-
-#Creation table des covariables des sites
-creer_infos_sites <-
-  CREATE TABLE infos_sites (
-    id_site VACHAR(15)
-    largeur_riviere INTEGER
-    temperature_eau REAL
-    transparence_eau VACHAR(15)
-    ETIQSTATION VACHAR(20)
-  
-    PRIMARY KEY (id_site)
-  )
-
-
-
-#Creation table des informations sur les id_especes
-creer_infos_especes <-
-  CREATE TABLE infos_especes (
-    id_espece VACHAR(100)
-    nom_sci VACHAR(150)
-    num_taxo VACHAR(20)
-    abondance INTEGER
-    fraction REAL
-    
-    PRIMARY KEY id_espece
-  )
-
-#VERSION 2 - OPTIMALE
-creer_especes_par_site <- 
-  CREATE TABLE especes_par_site (
-    
-    id_site VACHAR(100)
-    id_espece VACHAR(100)
+    nom_sci VACHAR(100)
     abondance INTEGER
     fraction REAL
     
     PRIMARY KEY (id_site, id_espece)
     FOREIGN KEY (id_site) REFERENCES infos_sites(id_site)
-    FOREIGN KEY (id_espece) REFERENCES infos_especes(id_espece)
   )
 
-#Creation table des covariables des sites
+#Creation table des sites
 creer_infos_sites <-
   CREATE TABLE infos_sites (
     id_site VACHAR(15)
     date_obs DATE
-    heure_obs 
+    heure_obs DATETIME
     site VACHAR(150)
     largeur_riviere INTEGER
-    temperature_eau REAL
+    profondeur_riviere INTEGER
+    vitesse_courant INTEGER
+    temperature_eau_c REAL
     transparence_eau VACHAR(15)
     ETIQSTATION VACHAR(20)
     
     PRIMARY KEY (id_site)
-  )
-
-
-
-#Creation table des informations sur les id_especes
-creer_infos_especes <-
-  CREATE TABLE infos_especes (
-    id_espece VACHAR(100)
-    nom_sci VACHAR(150)
-    num_taxo VACHAR(20)
-    
-    PRIMARY KEY (id_espece)
   )
 
 
