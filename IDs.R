df4c9823ccd26819d38da768b68c8127f0edaf35
LauraGlaude <laura_glaude@hotmail.com>
2024-03-10 16:59:49 -0400

    UpdateIDs2

--- IDs.R
+++ IDs.R
@@ -1,33 +1,26 @@
-#Afficher le répertoire de travail
-getwd()
-
-#Lecture de la table complète
-donnees <- read.csv('df.fus.csv')
-donnees
-
 #Installer le package dyplyr
 install.packages("dplyr")
 library(dplyr)
 
-                                              #Fonction#
+                                 #Fonction «traitement_donnees_IDs»#
 
-traitement_donnees_IDs <- function(df_propre) {
+traitement_donnees_IDs <- function(df.fus) {
   
 #Créer la colonne «ID date_site_heure_station»
-df_propre$IDsite <- paste(df_propre$date, df_propre$site, df_propre$heure_obs, df_propre$ETIQSTATION, sep = ';')
+df.fus$ID_date_site_heure_station <- paste(df.fus$date, df.fus$site, df.fus$heure_obs, df.fus$ETIQSTATION, sep = ';')
 
 #Créer la colonne «ID espèce_abondance_fraction»
-df_propre$IDespèce <- paste(df_propre$nom_sci, df_propre$abondance, df_propre$fraction, sep = ';')
+df.fus$ID_espèce_abondance_fraction <- paste(df.fus$nom_sci, df.fus$abondance, df.fus$fraction, sep = ';')
 
 #Attribuer des nombres uniques à chacun des IDs des deux colonnes
-df_propre$IDsite_unique <- match(df_propre$IDsite, unique(df_propre$IDsite))
-df_propre$IDespèce_unique <- match(df_propre$IDespèce, unique(df_propre$IDespèce))
+df.fus$ID_date_site_heure_station_unique <- match(df.fus$ID_date_site_heure_station, unique(df.fus$ID_date_site_heure_station))
+df.fus$ID_espèce_abondance_fraction_unique <- match(df.fus$ID_espèce_abondance_fraction, unique(df.fus$ID_espèce_abondance_fraction))
 
 #Mise en forme du tableau final
-df_propre$IDsite <- NULL
-df_propre$IDespèce <- NULL
-df_propre <- df_propre %>% rename('ID date_site_heure_station' = 'IDsite_unique')
-df_propre <- df_propre %>% rename('ID espèce_abondance_fraction' = 'IDespèce_unique')
+df.fus$ID_date_site_heure_station <- NULL
+df.fus$ID_espèce_abondance_fraction <- NULL
+df.fus <- df.fus %>% rename('ID date_site_heure_station' = 'ID_date_site_heure_station_unique')
+df.fus <- df.fus %>% rename('ID espèce_abondance_fraction' = 'ID_espèce_abondance_fraction_unique')
 df_propre <- df_propre[, c(1, 2, 3, 4, 14, 5, 6, 15, 7, 8, 9, 10, 11, 12, 13]
 #À ajuster selon le nombre de colonnes
 }
