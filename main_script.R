85c9c2ff758a79eacbf3ea978a8f4e97ad45ceeb
cloclo1008 <97540077+cloclo1008@users.noreply.github.com>
2024-03-10 17:01:58 -0400

    Update main_script.R

--- main_script.R
+++ main_script.R
@@ -9,20 +9,25 @@
   df_complet <- fusion_pls_fichiers(repertoire, type_fichier)
 
 
-# 2. Enlever les colonnes de NA et les doublons
-  source("Retirer_colonnes_GB1.R")
+# 2. Enlever les colonnes de NA et autres non-pertinentes
+  source("fonct_retirer_colonnes_GB1.R")
   df_propre <- Retirer_colonnes(df_complet) 
 
   
-# 3. Changer les types de donnees des colonnes pertinentes ***À voir si encore pertinent
-  source("Classer_colonnes_GB2.R")
+# 3. Enlever les lignes problématiques
+  source("fonct_enlever_lignes.R")
+  df_propre <- Enlever_lignes(df_propre)
+  
+  
+# 4. Changer les types de donnees des colonnes pertinentes ***À voir si encore pertinent
+  source("fonct_classer_colonnes_GB2.R")
   df_propre <- Classer_colonnes(df_propre)
 
 
-# 4. Créer des identifiants uniques pour chaque station par site pour une date et heure précises
+# 5. Créer des identifiants uniques pour chaque station par site pour une date et heure précises
   
 
-# 5. Séparer les données dans des tables dans une base de données relationnelle dans SQLite
+# 6. Séparer les données dans des tables dans une base de données relationnelle dans SQLite
   
   #Installer de package RSQLite permettant de se connecter au serveur SQLite
   install.packages("RSQLite")  
@@ -31,6 +36,6 @@
   bd <- dbConnect(RSQLite::SQLite(), dbname) #dbname correspond au chemin que l'ordinateur doit prendre pour retrouver le fichier que l'on veut cr?er. Donc, il s'agirait de setwd() en ajoutant ? la fin de se dernier le nom du fichier contenant les tables de donn?es et se terminant par .bd
   
   source("fonct_creation_bd.R")
-  tables_relation  <- creation_bd(df_propre, bd) #Spécifier le dataframe contenant les données à utiliser
+  tables_relation  <- creation_bd(df_propre, bd)
   
   dbDisconnect()
