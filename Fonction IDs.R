dc49e345a2690dc105594af0b21cfea0c1053359
gabriellebibeau <159631785+gabriellebibeau@users.noreply.github.com>
2024-03-10 18:08:52 -0400

    Update Fonction IDs.R

--- Fonction IDs.R
+++ Fonction IDs.R
@@ -2,17 +2,19 @@
 install.packages("dplyr")
 library(dplyr)
 
-                                 #Fonction «traitement_donnees_IDs»#
+                                 #Fonction Â«traitement_donnees_IDsÂ»#
 
-traitement_donnees_IDs <- function(df.fus) {
+traitement_donnees_IDs <- function(df_pp) {
   
-#Créer la colonne «ID date_site_heure_station»
-df_complet$ID_date_site_heure_station <- paste(df_complet$date, df_complet$site, df_complet$heure_obs, df_complet$ETIQSTATION, sep = ';')
+#CrÃ©er la colonne Â«ID date_site_heure_stationÂ»
+df_pp$ID_date_site_heure_station <- paste(df_pp$date, df_pp$site, df_pp$heure_obs, df_pp$ETIQSTATION, sep = ';')
 
-#Attribuer des nombres uniques à chacun des IDs des deux colonnes
-df_complet$ID_date_site_heure_station_unique <- match(df_complet$ID_date_site_heure_station, unique(df_complet$ID_date_site_heure_station))
+#Attribuer des nombres uniques Ã  chacun des IDs des deux colonnes
+df_pp$ID_date_site_heure_station_unique <- match(df_pp$ID_date_site_heure_station, unique(df_pp$ID_date_site_heure_station))
 
 #Mise en forme du tableau avec les IDs
-df_complet$ID_date_site_heure_station <- NULL
-df_complet <- df_complet %>% rename('ID date_site_heure_station' = 'ID_date_site_heure_station_unique')
+df_pp$ID_date_site_heure_station <- NULL
+df_pp <- df_pp %>% rename('id_site' = 'ID_date_site_heure_station_unique')
+
+  return(df_pp)
 }
