aa7ee453a72a0eb8e636fb89ec333a02d0bcf6a9
cloclo1008 <97540077+cloclo1008@users.noreply.github.com>
2024-03-15 14:41:24 -0400

    Update main_script.R

--- main_script.R
+++ main_script.R
@@ -27,28 +27,23 @@
 # 5. Créer des identifiants uniques pour chaque station par site pour une date et heure précises
   
   #Installer de package dplyr
-  install.packages("dplyr")
-  library(dplyr)
+  install.packages("dplyr") #si nécessaire
+  library(dplyr) ########rajouter dans la fonction?????
   
   source("fonct_ids.R")
   df_ID <- traitement_donnees_IDs(df_propre)
   
   
 #6. Créer les dataframes qui seront injectés dans SQLite
-  df_especes_par_site <- df_ID[, c("id_site","nom_sci","abondance", "fraction")]
-  df_infos_sites <- df_ID[, c("id_site","site", "date_obs", "heure_obs","largeur_riviere","profondeur_riviere","vitesse_courant","temperature_eau_c","transparence_eau","ETIQSTATION")]
+  df_especes <- df_ID[, c("id_site","nom_sci","abondance", "fraction")]
+  df_sites<- df_ID[, c("id_site","site", "date_obs", "heure_obs","largeur_riviere","profondeur_riviere","vitesse_courant","temperature_eau_c","transparence_eau","ETIQSTATION")]
   
   
 # 7. Créer les tables dans la base de données relationnelle dans SQLite
   
   #Installer de package RSQLite permettant de se connecter au serveur SQLite
-  install.packages("RSQLite")
-  library(RSQLite)
-  
-  #Connection de notre fichier de donnees au serveur SQLite
-  bd <- dbConnect(RSQLite::SQLite(), dbname) #dbname correspond au chemin que l'ordinateur doit prendre pour retrouver le fichier que l'on veut cr?er. Donc, il s'agirait de setwd() en ajoutant ? la fin de se dernier le nom du fichier contenant les tables de donn?es et se terminant par .bd
-  
+  install.packages("RSQLite") #si nécessaire
+
   source("fonct_creation_bd.R")
-  tables_relation  <- creation_bd(df_df_especes_par_site, df_infos_sites, bd)
+  tables_relation  <- creation_bd(df_especes, df_sites)
   
-  dbDisconnect()
