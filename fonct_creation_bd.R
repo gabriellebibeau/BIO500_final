1ee23752bcb550872f9c8317f36e82237f7bdc42
cloclo1008 <97540077+cloclo1008@users.noreply.github.com>
2024-03-15 14:41:27 -0400

    Update fonct_creation_bd.R

--- fonct_creation_bd.R
+++ fonct_creation_bd.R
@@ -1,18 +1,17 @@
 ### Fonction permettant de produire nos tables de données dans la base de données relationnelle
 
-
-# Objets à spécifier pour utiliser la fonction
-  #bd <- dbConnect(RSQLite::SQLite(), dbname) #dbname est le chemin vers le nouveau fichier crée contenant la base de données relationnelle
-  
-
-#Creation de la fonction
-creation_bd <- function(df.site, df.especes, bd){ #df.site/especes doivent être remplacer par les dataframes contenant les données à utiliser
+creation_bd <- function(df.site, df.especes){ #df.site/especes doivent être remplacer par les dataframes contenant les données à utiliser
   #Note: Le dataframe de données doit contenir les mêmes noms de colonnes que la base de données créée
 
+  library(RSQLite)
+  
+  #Connection de notre fichier de donnees au serveur SQLite
+  bd <- dbConnect(RSQLite::SQLite(), dbname = "bd_benthos") #dbname correspond au chemin que l'ordinateur doit prendre pour retrouver le fichier que l'on veut cr?er. Donc, il s'agirait de setwd() en ajoutant ? la fin de se dernier le nom du fichier contenant les tables de donn?es et se terminant par .bd
+  
   #Creer deux tables contenant dans l'ensemble toutes les donnees necessaires ? l'analyse, li?es entre elles par l'ID_site (cl? ?trang?re).
   #Table des sites
   creer_infos_sites <-
-    "CREATE TABLE infos_sites (
+    "CREATE TABLE sites (
     
       id_site             VARCHAR(15),
       site                VARCHAR(150),
@@ -32,7 +31,7 @@ creation_bd <- function(df.site, df.especes, bd){ #df.site/especes doivent être
   
    #Table des abondances d'espèces
     creer_especes_par_site <- 
-    "CREATE TABLE especes_par_site (
+    "CREATE TABLE especes (
      
       id_site   VARCHAR(100),
       nom_sci   VARCHAR(100),
@@ -43,11 +42,12 @@ creation_bd <- function(df.site, df.especes, bd){ #df.site/especes doivent être
       FOREIGN KEY (id_site) REFERENCES infos_sites(id_site)
       );"
     dbSendQuery(bd, creer_especes_par_site)
-  
 
     # Injection des enregistrements dans la base
-    dbWriteTable(bd, append = TRUE, name = "especes_par_site", value = df.especes, row.names = FALSE)
-    dbWriteTable(bd, append = TRUE, name = "infos_sites", value = df.site, row.names = FALSE)
+    dbWriteTable(bd, append = TRUE, name = "especes", value = df.especes, row.names = FALSE)
+    dbWriteTable(bd, append = TRUE, name = "sites", value = df.site, row.names = FALSE)
+    
+    dbDisconnect(bd)
     
     return(print("Good job!"))
 }
