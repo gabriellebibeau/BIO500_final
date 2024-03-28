2c687db7a9b1deae5b5f08d56f9df72be8988702
gabriellebibeau <bibeaugabrielle@gmail.com>
2024-03-28 16:33:39 -0400

    still_working_on_it

--- fonct_retirer_col.R
+++ fonct_retirer_col.R
@@ -1,16 +1,35 @@
 retirer_col <- function(table_fusionnee){ #La fonction a seulement besoin d'une matrice ou data frame quelconque pour fonctionner
   
-  table_fusionnee <- table_fusionnee[-1] #On enl?ve date car il est doubl? par date_obs
+  #1. Création d'une nouvelle table pour les colonnes utilisées
+  nv_data <- c(1:nrow(table_fusionnee)) 
   
-  nv_data <- c(1:nrow(table_fusionnee)) #Cr?ation d'une nouvelle table pour les colonnes utilis?es
-  
-  for (j in colnames(table_fusionnee)) { #boucle lisant chacune des colonnnes
-    if (sum(is.na(table_fusionnee[j])) < nrow(table_fusionnee)) { #Si les colonnes ont moins de NA de le nombre de lignes (donc contiennent minimalement une entr?e)
-      nv_data <- cbind(nv_data, table_fusionnee[j]) #Ajouter la colonnes de la table fusionn?e dans la nouvelle table
+  #2. Enlever les colonnes ne contenant que des NAs
+  for (k in colnames(table_fusionnee)) { #boucle lisant chacune des colonnnes
+    if(k == colnames(table_fusionnee[1])){ #si c'est la première colonne (qui est toujours pertinente ;) )
+      nv_data <- table_fusionnee[1] #mettre la première colonne dans la nouvelle première colonne
     }
-  }
+    
+    else{ #si ce n'est pas la première colonne de la table originale
+      
+      if(sum(is.na(table_fusionnee[k])) < nrow(table_fusionnee)) { #Si les colonnes ont moins de NA de le nombre de lignes (donc contiennent minimalement une entrée)
+        nv_data <- cbind(nv_data, table_fusionnee[k]) #Ajouter la colonnes de la table fusionnée dans la nouvelle table pertinente
+      }
+      
+    } #fin else
+  } #fin boucle for
   
-  nv_data <- nv_data[-1] #Enlever la premi?re colonne cr?er au d?but
-  return(nv_data) #la fonction retourne la nouvelle table r?duite
-}
+  return(nv_data) #la fonction retourne la nouvelle table réduite
+} #fin fonction
 
+#3. Enlever les colonnes doubles qui veulent dire la même choses 
+for (h in 1:ncol(nv_table)) { 
+  for (i in 1:ncol(nv_table)) {
+    for (j in 1:nrow(table_fusionnee)) {
+      
+      if(nv_table[j,h] = nv_table[j,i]) {
+        
+      }
+      
+    } #fin for j
+  } #fin for i
+} #fin for h
