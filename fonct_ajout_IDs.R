27804c97d9824b3790d0be02045c3eaeb1594072
cloclo1008 <tanc1401@usherbrooke.ca>
2024-03-29 18:41:43 -0400

    Raccourcir noms objets

--- fonct_ajout_IDs.R
+++ fonct_ajout_IDs.R
@@ -1,19 +1,18 @@
 
                                  #Fonction «traitement_donnees_IDs»#
 
-ajout_IDs <- function(df_pp) {
+ajout_IDs <- function(df) {
   
   library(dplyr)
   
   #Créer la colonne «ID date_site_heure_station»
-  df_pp$ID_date_site_heure_station <- paste(df_pp$date, df_pp$site, df_pp$heure_obs, df_pp$ETIQSTATION, sep = ';')
-
+  df$infos_ids <- paste(df$date, df$site, df$heure_obs, df$ETIQSTATION, sep = ';')
+  
   #Attribuer des nombres uniques à chacun des IDs des deux colonnes
-  df_pp$ID_date_site_heure_station_unique <- match(df_pp$ID_date_site_heure_station, unique(df_pp$ID_date_site_heure_station))
+  df$id_site <- match(df$infos_ids, unique(df$infos_ids))
+  
+  #Retirer la colonne infos_ids
+  df$infos_ids <- NULL
 
-  #Mise en forme du tableau avec les IDs
-  df_pp$ID_date_site_heure_station <- NULL
-  df_pp <- df_pp %>% rename('id_site' = 'ID_date_site_heure_station_unique')
-
-  return(df_pp)
+  return(df)
 }
