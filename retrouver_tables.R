a6567809fbc56db2fcb84404ff73cad3d8c743e3
cloclo1008 <tanc1401@usherbrooke.ca>
2024-03-30 00:18:21 -0400

    Update retrouver_tables.R

--- retrouver_tables.R
+++ retrouver_tables.R
@@ -4,39 +4,28 @@
 repertoire <- ("C:/Users/cloet/Desktop/BIO500_final/benthos") #Spécifier le chemin vers le dossier contenant les fichiers
 
 #Importer et appliquer la fonction 
-source("fonct_fus_fichiers.R")
+source("fonct_fus_fichiers.r")
 df_complet <- fus_fichiers(repertoire)
 
+# 2. Nettoyer les données
+source("fonct_nettoyage.R")
+df_propre <- nettoyage(df_complet) 
 
-# 2. Enlever les colonnes de NA et autres non-pertinentes
-source("fonct_retirer_col_2.0.R")
-df_propre <- retirer_col(df_complet) 
-
-
-# 3. Remplacer les données problématiques par des NA
-source("fonct_remplacer_NA.R")
-df_classer <- remplacer_NA(df_propre)
-
-
-# 4. Changer les types de donnees des colonnes pertinentes ***À voir si encore pertinent
+# 3. Changer les types de donnees des colonnes
 source("fonct_classer_col.R")
-df_propre <- classer_col(df_propre)
+df_classe <- classer_col(df_propre)
 
+# 4. Créer des identifiants uniques pour chaque station par site pour une date et heure précises
 
-# 5. Créer des identifiants uniques pour chaque station par site pour une date et heure précises
-
-library(dplyr) ########rajouter dans la fonction?????
-
+#Installer de package dplyr si besoin
 source("fonct_ajout_IDs.R")
-df_ID <- ajout_IDs(df_propre)
+df_IDs <- ajout_IDs(df_classe)
 
+# 5. Créer les dataframes qui seront injectés dans SQLite
+df_especes <- df_IDs[, c("id_site","nom_sci","abondance", "fraction")]
+df_sites<- df_IDs[, c("id_site","site", "date", "heure_obs","largeur_riviere","profondeur_riviere","vitesse_courant","temperature_eau_c","transparence_eau","ETIQSTATION")]
 
-#6. Créer les dataframes qui seront injectés dans SQLite
-df_especes <- df_ID[, c("id_site","nom_sci","abondance", "fraction")]
-df_sites<- df_ID[, c("id_site","site", "date_obs", "heure_obs","largeur_riviere","profondeur_riviere","vitesse_courant","temperature_eau_c","transparence_eau","ETIQSTATION")]
-
-
-# 7. Créer les tables dans la base de données relationnelle dans SQLite
+# 6. Créer les tables dans la base de données relationnelle dans SQLite
 
 source("fonct_creation_bd.R")
 tables_relation  <- creation_bd(df_sites, df_especes)
@@ -44,6 +33,7 @@ tables_relation  <- creation_bd(df_sites, df_especes)
 bd <- dbConnect(RSQLite::SQLite(), dbname = "bd_benthos.bd")
 dbSendQuery(bd, "DROP TABLE sites")
 dbSendQuery(bd, "DROP TABLE especes")
+dbDisconnect(bd)
 
 dbGetQuery(bd, 'SELECT * FROM sites LIMIT 4')
 dbGetQuery(bd, 'SELECT * FROM especes LIMIT 4')
