d0ccef3337665b13072bb65a8bafc23e4c4daafa
cloclo1008 <tanc1401@usherbrooke.ca>
2024-03-30 00:34:59 -0400

    Update retrouver_tables.R

--- retrouver_tables.R
+++ retrouver_tables.R
@@ -35,5 +35,7 @@ dbSendQuery(bd, "DROP TABLE sites")
 dbSendQuery(bd, "DROP TABLE especes")
 dbDisconnect(bd)
 
+bd <- dbConnect(RSQLite::SQLite(), dbname = "bd_benthos.bd")
 dbGetQuery(bd, 'SELECT * FROM sites LIMIT 4')
 dbGetQuery(bd, 'SELECT * FROM especes LIMIT 4')
+dbDisconnect(bd)
