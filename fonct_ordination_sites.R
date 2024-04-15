8587500cab72312bda40121ebbafbf80480647c3
gabriellebibeau <159631785+gabriellebibeau@users.noreply.github.com>
2024-04-14 21:10:48 -0400

    Finie

--- fonct_ordination_sites.R
+++ fonct_ordination_sites.R
@@ -1,50 +1,79 @@
-#Librairies et donn�es ----
+#Librairies 
 library(segregation)
+library(vegan)
+library(dplyr)
+library(ggplot2)
+library(glue)
 
-table_abondance
+################# FONCTION ORDINATION EFFET SITES ######################
 
-#Table de dissimilarit� ----
+ordination_sites <- function(tbl_abd){ #on rentre la table d'abondance
+  
+  #1. Boucle pour enlever NA
+  
+  for (j in 1:ncol(tbl_abd)) {
+    for (i in 1:nrow(tbl_abd)) { #pour toutes les données...
+      
+      if(is.na(tbl_abd[i,j]) == TRUE){ #Si c'est un NA,
+        tbl_abd <- tbl_abd[-i,] #L'enlever
+      }
+      
+    }#fin boucle for i
+  }#fin boucle for j
+  
+  
+  #2. Boucle pour faire une nouvelle matrice d'abondance
+  
+  mat_abd <- matrix(0, nrow = length(unique(tbl_abd$id_site)), ncol = length(unique(tbl_abd$nom_sci)), 
+                    dimnames = list(unique(tbl_abd$id_site), unique(tbl_abd$nom_sci))) #matrice vide
+  
+  for (j in 1:ncol(mat_abd)) {
+    for (i in 1:nrow(mat_abd)) { #pour toutes les données...
+      
+      #mettre la bonne abondance pour le bon id de site et unités taxonomiques observées
+      try(mat_abd[i,j] <- tbl_abd$abondance_ajust[tbl_abd$id_site == rownames(mat_abd)[i] & 
+                                                    tbl_abd$nom_sci == colnames(mat_abd)[j]])
+      
+    }#fin boucle for i
+    
+  }#fin boucle for j
+  
+  #3. Matrice de distance Bray-Curtis
+  dist_Bray <- vegdist(mat_abd, method="bray")
+  
+  #4. Ordination
+  
+  #4.1 Points ordination
+    pcoa <- cmdscale(dist_Bray, k=2, eig = TRUE) #Faire la PCoA
+    pts <- as.data.frame(pcoa$points) #Extraire les points
+    colnames(pts) <- c('pcoa1', 'pcoa2') #changer les noms de colonnes des points
+  
+  #4.2 Pourcentage d'explication
+    pourcentage <- 100*pcoa$eig / sum(pcoa$eig) #trouver les pourcentages explicatifs
+    prct_axe1 <- round(pourcentage[1], digits = 1) #Extraire pour le 1er axe
+    prct_axe2 <- round(pourcentage[2], digits = 1) #Extraire pour le 2e axe
+  
+  #4.3 Nom de sites dans le data frame des points
+    site <- vector(length = nrow(pts)) #vecteur vide
+  
+    for (i in 1:nrow(pts)) { #boucle pour mettre le bon nom de site au bon id_site
+    
+      site[i] <- unique(tbl_abd$site[tbl_abd$id_site == rownames(pts)[i]])
+    
+    }#fin boucle for i
+  
+    pts_site <- cbind(pts, site)
+  
+  #4.4 Visualisation avec ggplot2
+    nom_axe <- c(glue('PCo 1 ({prct_axe1}%)'), glue('PCo 2 ({prct_axe2}%)')) #Noms des axes récurssifs
+  
+    ordination <- ggplot(data = pts_site, aes(x = pcoa1, y = pcoa2, color = site)) + #le graphique
+      geom_point() +
+      stat_ellipse() +
+      labs(x = nom_axe[1], y = nom_axe[2]) +
+      theme_minimal()
+    
+    return(ordination)
+  
+} #fin fonction
 
-
-
-
-
-
-
-#### exemple Sarah ####
-# Prepare for ordination for all treatments
-comm.phylo16S.mat <- vegdist(phylo16S.mat , "bray")
-PCoA.comm.phylo16S.mat<-capscale(phylo16S.mat~1,distance="bray")
-PCoA.comm.phylo16S.mat$CA$eig[1:3]/sum(PCoA.comm.phylo16S.mat$CA$eig)
-eig <- PCoA.comm.phylo16S.mat$CA$eig
-eig[1]/sum(abs(eig)) # 13.51 %
-eig[2]/sum(abs(eig)) # 8.56 %
-eig[3]/sum(abs(eig)) # 5.25 %
-PCoA.phylo16S<- PCoA.comm.phylo16S.mat # New shorter name
-
-# Assign PCoA scores of each samples as column in the meta dataframe
-meta16S$MDS1 <- vegan::scores(PCoA.phylo16S)$site[,1] # MDS1
-meta16S$MDS2 <- scores(PCoA.phylo16S)$site[,2] # MDS2
-head(meta16S)
-
-pcoa_cities_16S <- ggplot(meta16S, aes(MDS1, MDS2, shape = city, fill = city, color = city))+# What we want to plot
-  # Add ellipses representing the 95% confidence level for each site
-  stat_ellipse(level=0.95, geom = "polygon",alpha = 1/3, aes(group = city))+
-  geom_point(aes(size=0.1))+ # Add points to the plot, specify point size
-  # Customize the color scale for site labels
-  scale_color_manual(name = "Cities",values= c("black","black","black","black"))+
-  # Customize the shape scale for cultivar labels
-  scale_shape_manual(name = "Cities", values = c(22,21,24))+
-  # Customize the fill color scale for site labels
-  scale_fill_manual(name = "Cities",values = c("indianred3","khaki1","darkolivegreen3"))+
-  theme_bw()+ # Set black and white theme
-  ggtitle("PCoA of bacteria along cities") + # Set the plot title
-  theme(plot.title = element_text(hjust = 0.5, face="bold"),
-        axis.title = element_text(size = 16),
-        legend.title = element_text(size=16),
-        legend.text = element_text(size=14))+ # Customize plot and axis titles
-  xlab("PCoA1 (14%)") +  # Set the x-axis label
-  ylab("PCoA2 (9%)")+ # y-axis label
-  guides(size = "none")+
-  guides(shape = guide_legend(override.aes = list(size=5)))+
-  guides(fill = guide_legend(override.aes = list(shape = NA))); pcoa_cities_16S # Remove the size legend
